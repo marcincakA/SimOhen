e06c8236cef3545e1c8ad8e207cbc9c33c5ea119
marcincakA <marcincaka@gmail.com>
2024-01-03 16:54:01 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/SimOhenKlient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimOhenKlient.dir/main.cpp.o"
+  "SimOhenKlient"
+  "SimOhenKlient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimOhenKlient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
