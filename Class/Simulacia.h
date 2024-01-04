1ad7c41c10d9b0d91c131279cb43a01b7ebd6b8a
marcincakA <marcincaka@gmail.com>
2024-01-04 12:36:57 +0100

    step logic

--- Class/Simulacia.h
+++ Class/Simulacia.h
@@ -116,6 +116,299 @@ public:
         }
     }
 
+    void windStep() {
+        if(this->vietor != BEZVETRIE) {
+            //vietor drzi svoj smer na tri tahy
+            if (this->zaciatokVetra == this->pocetSimulacii - 3) {
+                this->vietor = BEZVETRIE;
+                return;
+            }
+
+            if(this->vietor == BEZVETRIE) {
+                std::random_device rd;
+                std::mt19937 gen(rd());
+                std::uniform_int_distribution<> dis(0, 100);
+                int cislo = dis(gen);
+                if (cislo <= 90) {
+                    this->vietor = BEZVETRIE;
+                } else {
+                    int smerVetra = dis(gen);
+                    if (smerVetra <= 25) {
+                        this->vietor = SEVER;
+                    } else if (smerVetra <= 50) {
+                        this->vietor = VYCHOD;
+                    } else if (smerVetra <= 75) {
+                        this->vietor = JUH;
+                    } else {
+                        this->vietor = ZAPAD;
+                    }
+                    this->zaciatokVetra = this->pocetSimulacii;
+                }
+            }
+
+
+        }
+    }
+
+    void setFireArround(int x, int y) { // x je vyska y je sirka
+        //metodka co zoberie stav vetra a bunku na zaklade vetra zapali ostatne bunky
+        if (!positionIsValid(x, y)) {
+            std::cout << "Zadana pozicia nie je validna" << std::endl;
+            return;
+        }
+        if (!biotop[x][y].getStav() == POZIAR) {
+            std::cout << "Bunka nehori" << std::endl;
+            return;
+        }
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 100);
+        //*******************BEZVETRIE*******************//
+        if (this->vietor == BEZVETRIE) {
+            if (positionIsValid(x - 1, y)) {
+                if (biotop[x - 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x + 1, y)) {
+                if (biotop[x + 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y - 1)) {
+                if (biotop[x][y - 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y + 1)) {
+                if (biotop[x][y + 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+        }
+        /************************SEVER**********************/
+        else if(this->vietor == SEVER) {
+            if (positionIsValid(x - 1, y)) {
+                if (biotop[x - 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 90) { // 90 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x + 1, y)) {
+                if (biotop[x + 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 2) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y - 1)) {
+                if (biotop[x][y - 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar ostava ako pri bezvetri
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y + 1)) {
+                if (biotop[x][y + 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar ostava ako pri bezvetri
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+        /************************VYCHOD**********************/
+        } else if(this->vietor == VYCHOD) {
+            if (positionIsValid(x - 1, y)) {
+                if (biotop[x - 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x + 1, y)) {
+                if (biotop[x + 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y - 1)) {
+                if (biotop[x][y - 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 2) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y + 1)) {
+                if (biotop[x][y + 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 90) { // 90 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+        /************************JUH**********************/
+        } else if(this->vietor == JUH) {
+            if (positionIsValid(x - 1, y)) {
+                if (biotop[x - 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 2) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x + 1, y)) {
+                if (biotop[x + 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 90) { // 90 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y - 1)) {
+                if (biotop[x][y - 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y + 1)) {
+                if (biotop[x][y + 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 90 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+        /************************ZAPAD**********************/
+        } else if(this->vietor == ZAPAD) {
+            if (positionIsValid(x - 1, y)) {
+                if (biotop[x - 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 20 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x + 1, y)) {
+                if (biotop[x + 1][y].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 20) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y - 1)) {
+                if (biotop[x][y - 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 90) { // 90 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+            if (positionIsValid(x, y + 1)) {
+                if (biotop[x][y + 1].isFlamable()) {
+                    int pravdepodobnostPoziaru = dis(gen);
+                    if (pravdepodobnostPoziaru <= 2) { // 2 percentna sanca na poziar
+                        biotop[x - 1][y].setStav(POZIAR);
+                    }
+                }
+            }
+        }
+    }
+
+    Biotop* findBiotopNear(int x, int y,BiotopEnum biotopEnum) {
+        if (!positionIsValid(x, y)) {
+            std::cout << "Zadana pozicia nie je validna" << std::endl;
+            return nullptr;
+        }
+        //pozri okolo
+        if (positionIsValid(x - 1, y)) {
+            if (biotop[x - 1][y].getStav() == biotopEnum) {
+                return &biotop[x - 1][y];
+            }
+        }
+        if (positionIsValid(x + 1, y)) {
+            if (biotop[x + 1][y].getStav() == biotopEnum) {
+                return &biotop[x + 1][y];
+            }
+        }
+        if (positionIsValid(x, y - 1)) {
+            if (biotop[x][y - 1].getStav() == biotopEnum) {
+                return &biotop[x][y - 1];
+            }
+        }
+        if (positionIsValid(x, y + 1)) {
+            if (biotop[x][y + 1].getStav() == biotopEnum) {
+                return &biotop[x][y + 1];
+            }
+        }
+        return nullptr;
+    }
+
+    void regenerateBiotop(int x, int y) {
+        if (!positionIsValid(x,y)) {
+            std::cout << "Zadana pozicia nie je validna" << std::endl;
+            return;
+        }
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 100);
+        int pravdepodobnost = dis(gen);
+        if (this->biotop[x][y].getStav() == ZHORENA) {
+            Biotop* foundBiotop = findBiotopNear(x, y, VODA);
+            if (foundBiotop != nullptr) {
+                if (pravdepodobnost <= 10) {
+                    this->biotop[x][y].setStav(LUKA);
+                }
+            }
+        }
+        if (this->biotop[x][y].getStav()==LUKA) {
+            Biotop* foundBiotop = findBiotopNear(x, y, LES);
+            if (foundBiotop != nullptr) {
+                if (pravdepodobnost <= 2) {
+                    this->biotop[x][y].setStav(LES);
+                }
+            }
+        }
+    }
+
+
+    void step() {
+        windStep(); // chod vetra
+        //find fire
+        for(int i = 0; i < sizeX; i++) {
+            for(int j = 0; j < sizeY; j++) {
+                if (biotop[i][j].getStav() == POZIAR) {
+                    setFireArround(i, j);
+                    biotop[i][j].setStav(ZHORENA);
+                }
+                regenerateBiotop(i, j);
+            }
+        }
+    }
+
 };
 
 
